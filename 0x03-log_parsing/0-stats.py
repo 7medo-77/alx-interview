6fc2b53ebc1e0a899a6aa42483c4b1fe295d80c6
hmedo <hmedo@7medo>
2024-08-17 22:42:39 +0300

    0-stats.py_22-42

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,8 +17,6 @@ def int_handler(dict_status, size):
         print("{}: {}".format(key, dict_status[key]))
 
 for curr_line in sys.stdin:
-    if not sys.stdin.readline():
-        break
     regexP = re.compile(r'(\d{1,3}\.){3}\d{1,3}\s\-\s\[.*\]\s\"[A-Z]{1,7}\s/.*/[0-9]{1,4}\sHTTP/\d\.\d\"\s(\d{3})\s(\d{1,4})')
     match = regexP.match(curr_line)
 
@@ -30,12 +28,12 @@ for curr_line in sys.stdin:
             status_dict[line_code] += 1
         else:
             status_dict[line_code] = 1
-
-        if (line_no % 10 == 0):
-            # print("line No: {}".format(line_no))
-            print("File size: {}".format(file_size))
-            for key in sorted(status_dict.keys()):
-                print("{}: {}".format(key, status_dict[key]))
         # signal.signal(signal.SIGINT, int_handler(status_dict, file_size))
-        signal.signal(signal.SIGINT, lambda signum, frame: int_handler(status_dict, file_size))
+
+    if (line_no % 10 == 0):
+        # print("line No: {}".format(line_no))
+        print("File size: {}".format(file_size))
+        for key in sorted(status_dict.keys()):
+            print("{}: {}".format(key, status_dict[key]))
     line_no += 1
+    signal.signal(signal.SIGINT, lambda signum, frame: int_handler(status_dict, file_size))
