84f6242a118bb1816599a6311ca10bacd4b630ac
hmedo <hmedo@7medo>
2024-08-28 18:33:16 +0300

    sol_18-33

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -13,3 +13,9 @@ print(validUTF8(data))
 
 data = [229, 65, 127, 256]
 print(validUTF8(data))
+
+data = [235, 140]
+print(validUTF8(data))
+
+data =  [250, 145, 145, 145, 145]
+print(validUTF8(data))
