b593deeba7560811f1515d07a806f43e20ca2857
hmedo <hmedo@7medo>
2024-09-12 15:08:59 +0300

    0-starwars_characters.js_15-08

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,9 +3,9 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-async function enumerateCharactersByOrder(arrayOfLinks) {
-  for (link of arrayOfLinks) {
-    let name = await fetch(link)
+async function enumerateCharactersByOrder (arrayOfLinks) {
+  for (const link of arrayOfLinks) {
+    const name = await fetch(link)
       .then(result => result.json())
       .then(jsonRes => jsonRes.name);
     console.log(name);
@@ -15,12 +15,11 @@ async function enumerateCharactersByOrder(arrayOfLinks) {
 // let charArray;
 
 request(url, (error, response, body) => {
-    if (!error) {
-      result = JSON.parse(body);
-      let charArray = result.characters;
-      enumerateCharactersByOrder(charArray)
-    } else {
-      console.error('Error:', error);
-    }
-  })
-  // console.log(charArray);
+  if (!error) {
+    const result = JSON.parse(body);
+    const charArray = result.characters;
+    enumerateCharactersByOrder(charArray);
+  } else {
+    console.error('Error:', error);
+  }
+});
